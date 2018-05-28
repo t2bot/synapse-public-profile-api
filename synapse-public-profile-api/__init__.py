e282096c9a0bb56d965fac74aab9303231142461
Travis Ralston <travpc@gmail.com>
2018-05-28 12:22:12 -0600

    Initial concept for returning a real profile

--- synapse-public-profile-api/__init__.py
+++ synapse-public-profile-api/__init__.py
@@ -10,10 +10,15 @@ class ProfileRequestHandler(object):
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
         if 'user_id' not in request.args or len(request.args['user_id']) != 1:
             request.setResponseCode(400)
-            request.write('{"errcode":"M_BAD_REQUEST","error":"Missing user_id query param"}')
+            request.write('{"errcode":"IO.T2BOT.PROFILE_BAD_REQUEST","error":"Missing user_id query param"}')
         else:
             user_id = request.args['user_id'][0]
-            request.write(user_id)
+            if user_id not in self._config["allowed_users"]:
+                request.setResponseCode(400)
+                request.write('{"errcode":"IO.T2BOT.PROFILE_BAD_REQUEST","error":"That user is not allowed"}')
+            else:
+                profile = self._module_api.hs.get_profile_handler().get_profile(user_id)
+                request.write(json.dumps(profile))
         request.finish()
 
     @staticmethod
